4acb5c50262927f3114364deb2d93529be29b6a5
Ayoub Souinia <asouinia@e2r12p7.1337.ma>
2021-11-06 20:29:27 +0100

    probably memcpy is Okay

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,8 +17,9 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 	size_t	i;
 
 	i = -1;
-	// while (++i < n && (dst || src))
-	while (++i < n && dst && src)
+	if (!dst && !src)
+		return (NULL);
+	while (++i < n)
 	{
 		*((unsigned char *)(dst + i)) = *((unsigned char *)(src + i));
 	}
