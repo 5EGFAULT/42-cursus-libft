d6d5ad06681a761346104e6fe7497c177d6a70a6
Ayoub Souinia <asouinia@e1r10p11.1337.ma>
2021-11-15 23:22:03 +0100

    few bonus functions to go

---
+++ ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asouinia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 23:15:17 by asouinia          #+#    #+#             */
+/*   Updated: 2021/11/15 23:15:18 by asouinia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!new || !lst)
+		return ;
+	last = ft_lstlast(*lst);
+	new->next = NULL;
+	last->next = new;
+}
